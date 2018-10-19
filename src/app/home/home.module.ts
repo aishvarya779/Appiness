b9f245e9664e9896d6a212399aaea2f60f2aace5
aishvarya779 <aishvaryatiwari123@gmail.com>
2018-10-20 01:54:04 +0530

    dunamic component loading done

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ],
-  declarations: [HomeComponent]
+  imports: [CommonModule, HomeRoutingModule],
+  declarations: [HomeComponent, UserListComponent, UserProfileComponent],
+  entryComponents: [UserListComponent, UserProfileComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+  ComponentFactoryResolver
+} from '@angular/core';
 import { HomeService } from '../home.service';
 import { MessageService } from '../../shared/services/message.service';
 import { UserAuthService } from '../../user-auth.service';
+import { UserListComponent } from '../user-list/user-list.component';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
 
 @Component({
   selector: 'appi-home',
@@ -9,17 +17,36 @@ import { UserAuthService } from '../../user-auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  public componentRef;
+  @ViewChild('dynamicComponent', { read: ViewContainerRef })
+  container;
   constructor(
     private _homeService: HomeService,
     private _msgService: MessageService,
-    private _userAuth: UserAuthService
+    private _userAuth: UserAuthService,
+    private resolver: ComponentFactoryResolver
   ) {}
 
   ngOnInit() {
     this._userAuth.userData$.subscribe(val => {
       console.log(val);
+      if (val && val.role === 'Admin') {
+        const factory = this.resolver.resolveComponentFactory(
+          UserListComponent
+        );
+        this.componentRef = this.container.createComponent(factory);
+      } else {
+        const factory = this.resolver.resolveComponentFactory(
+          UserProfileComponent
+        );
+        this.componentRef = this.container.createComponent(factory);
+      }
     });
     // this.getAllUsers();
+  }
+
+  ngOnDestroy() {
+    this.componentRef.destroy();
   }
 
   public getAllUsers() {
