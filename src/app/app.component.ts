4e23b826f450dc28f418c5c8b7001060d6403f53
aishvarya779 <aishvaryatiwari123@gmail.com>
2018-10-20 17:15:36 +0530

    loading for route change done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  Router,
+  Event,
+  NavigationStart,
+  NavigationEnd,
+  NavigationError,
+  NavigationCancel
+} from '@angular/router';
 
 @Component({
   selector: 'appi-root',
@@ -7,4 +15,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Appiness';
+  private isLoading = false;
+  constructor(private _router: Router) {
+    this._router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.isLoading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.isLoading = false;
+    }
+    if (event instanceof NavigationCancel) {
+      this.isLoading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.isLoading = false;
+    }
+  }
 }
